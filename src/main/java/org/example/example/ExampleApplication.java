0c2821b549342184e60ece9a27f8b5bd5b34f8ba
Heo-Gyeom <heogyeom80@gmail.com>
2026-02-27 11:34:59 +0900

    fix controller scanning with ComponentScan, add mapping logs, tomcat-embed-jasper

--- src/main/java/org/example/example/ExampleApplication.java
+++ src/main/java/org/example/example/ExampleApplication.java
@@ -2,8 +2,10 @@ package org.example.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class ExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args);
